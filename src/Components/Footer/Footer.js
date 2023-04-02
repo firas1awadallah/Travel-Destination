c8bede61fb80b07090fe57ceddfa71ddd7fadb4d
firas1awadallah <firasawa@gmail.com>
2023-04-02 07:56:39 +0000

    first-react

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,12 @@
+function Footer (){
+    return (
+        <>
+        <h2>Firas Awadallah</h2>
+     <a href="facebook.com">Facebook</a><br></br>
+     <a href="linkedin.com">Linkedin</a><br></br>
+     <a href="google.com">Google</a><br></br>
+    
+       </>
+          )
+    }
+    export default Footer;
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+function Header (){
+return (
+    <>
+    <div>
+<h1>This is my Header</h1>
+<hr></hr>
+    </div> 
+    </>
+)
+}
+export default Header;
